9a7e5a6e32f5a2e823fd330991b486972b6de1d4
PaPer Li <paperli@PaPer-Liteki-MacBook-Pro.local>
2012-09-11 01:00:30 +0200

    add ball_update code
    the player owned right_of_serve calculated the ball movement and emit
    event "ball_update" to server
    
    another player received the event "ball_updated" and update the ball
    position from server
    
    take look at the screenshots folder

--- simplest.app.js
+++ simplest.app.js
@@ -12,6 +12,7 @@ var server = http.createServer(app);
 var sio = io.listen(server);
 
 var players = [];
+var games = [];
 
 server.listen(gameport);
 
@@ -53,22 +54,38 @@ sio.sockets.on('connection',function(client){
 	
 	client.on('disconnect', function(){
 		console.log('\t socket.io:: client disconnected ' + client.userid);
+		if (client.gameid > -1){
+			// shut down game match
+			games[client.gameid].player0.emit('game_interrupted',{error:'your opponent left the game'});
+			games[client.gameid].player1.emit('game_interrupted',{error:'your opponent left the game'});
+		}
+		
 		if (players.indexOf(client) >= 0){
 			players.splice(players.indexOf(client), 1);
 		}
+		
 	});
 	
 	if (players.length%2 == 0 && players.length > 0){
 		//in pair
 		console.log('\t '+players.length+' players are in pair');
-		client.gameid = UUID();
+		client.gameid = games.length;
 		players[players.indexOf(client)-1].gameid = client.gameid;
 		client.emit("gameStart",{ gid: client.gameid, player:1, opponent:players[players.indexOf(client)-1].userid });
 		players[players.indexOf(client)-1].emit("gameStart",{ gid: client.gameid, player:0, opponent:client.userid });
+		games.push({id:client.gameid,
+					player0:players[players.indexOf(client)-1],
+					player1:client});
 		client.on('ball_update',function(data){
 			//console.log('\t socket.io:: ball x: '+data.ball.x+' y: '+data.ball.y);
 			//player1 is owning rightOfServe. passing to player0 (with ball_updated event)
-			players[players.indexOf(client)-1].emit('ball_updated',{data:data});
+			players[players.indexOf(client)-1].emit('ball_updated',data);
+		});
+		
+		players[players.indexOf(client)-1].on('ball_update',function(data){
+			//console.log('\t socket.io:: ball x: '+data.ball.x+' y: '+data.ball.y);
+			//player1 is owning rightOfServe. passing to player0 (with ball_updated event)
+			client.emit('ball_updated',data);
 		});
 	}
 	
